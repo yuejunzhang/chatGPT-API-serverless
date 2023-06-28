460742ea346307be12692b3023704c6489ae791b
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2023-06-28 10:37:21 +0800

    Update chatGPT-API-Function.go

--- api/chatGPT-API-Function.go
+++ api/chatGPT-API-Function.go
@@ -1,13 +1,17 @@
 package handler
 
 import (
+	// "bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+
+	// "io/ioutil"
 	"math/rand"
 	"net/http"
-	// "net/http/httptest"
+
+	// "net/http/httptest" //仅仅在开发环境配合main()测试函数，生产环境注释掉
 	"os"
 	"strconv"
 	"strings"
@@ -94,7 +98,27 @@ var (
 )
 
 // /////////////////////////////////////////////////////无服务器函数/////////////////////////////////////////////////////////////////////////////
+// func main() { //仅仅在开发环境测试函数，生产环境注释掉
+// 	// Init()
+// 	// 创建一个响应记录器
+// 	ww := httptest.NewRecorder()
+// 	// 创建一个 JSON 字符串作为请求体
+// 	requestBody := `{
+// 		"model":  "gpt-3.5-turbo",
+//     	"messages": [{"role": "user", "content": "你是谁"}],
+// 		"stream": true
+// 		}`
+// 	// 创建一个请求对象
+// 	rr := httptest.NewRequest("POST", "http://www.chat.openai.com", bytes.NewBuffer([]byte(requestBody)))
 
+// 	rr.Header.Add("Content-Type", "application/json")
+// 	rr.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
+// 	rr.Header.Add("Authorization", "Bearer "+"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJha3Vua2VqaUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZX0sImh0dHBzOi8vYXBpLm9wZW5haS5jb20vYXV0aCI6eyJ1c2VyX2lkIjoidXNlci1yUTBOejFGa2d5VkJvblZTUDk1WnpWdlIifSwiaXNzIjoiaHR0cHM6Ly9hdXRoMC5vcGVuYWkuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTE1NTA3NTE1MjAzNDIwNDY0NDkzIiwiYXVkIjpbImh0dHBzOi8vYXBpLm9wZW5haS5jb20vdjEiLCJodHRwczovL29wZW5haS5vcGVuYWkuYXV0aDBhcHAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY4Nzc2NTAxMywiZXhwIjoxNjg4OTc0NjEzLCJhenAiOiJUZEpJY2JlMTZXb1RIdE45NW55eXdoNUU0eU9vNkl0RyIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgbW9kZWwucmVhZCBtb2RlbC5yZXF1ZXN0IG9yZ2FuaXphdGlvbi5yZWFkIG9yZ2FuaXphdGlvbi53cml0ZSJ9.iIJJaNbv_uWzB2MSlHrmJ293qEvLcfOWYPEO_EjuPTvjj7EF1otQMfz2ppj3_blKfvxE4zGLZhTLOdIdY278WzZ6lXvqekQ-8HbdW90peHVgqgf2Kj1kKbZkSO-aDuhoerCUxnp1KE4oBd6Hp29BFWgRyCx2senox2TM7canm___VB8p0uNGw9w1At35o2OCWNZ11oE90zvZU_SPoe-Vcx1LITZrWhCC48WjyPMGlB1ghentYDeJMTThxJbb1vFLT6tiPEHmynALO4aW6_GFAm02UkCE4CVSv5O2aLxMOK0LLu4HuQ5R6HmCn3Xedbnwrk556OnfFtwmT8IV83mkwA")
+// 	// 调用处理函数
+// 	// rr.Header.Add("Authorization", "Bearer "+"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJha3Vua2VqaUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZX0sImh0dHBzOi8vYXBpLm9wZW5haS5jb20vYXV0aCI6eyJ1c2VyX2lkIjoidXNlci1yUTBOejFGa2d5VkJvblZTUDk1WnpWdlIifSwiaXNzIjoiaHR0cHM6Ly9hdXRoMC5vcGVuYWkuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTE1NTA3NTE1MjAzNDIwNDY0NDkzIiwiYXVkIjpbImh0dHBzOi8vYXBpLm9wZW5haS5jb20vdjEiLCJodHRwczovL29wZW5haS5vcGVuYWkuYXV0aDBhcHAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY4Njk2MDg1NiwiZXhwIjoxNjg4MTcwNDU2LCJhenAiOiJUZEpJY2JlMTZXb1RIdE45NW55eXdoNUU0eU9vNkl0RyIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgbW9kZWwucmVhZCBtb2RlbC5yZXF1ZXN0IG9yZ2FuaXphdGlvbi5yZWFkIG9yZ2FuaXphdGlvbi53cml0ZSJ9.kqdFg7UlvzNxNm1ns6hVDzb6UhvmRB9cf9xbjHHlY5FXXaO_dqE1qxZPOeq_bHk081bRxgXByYukxYMk8CAf6IrXZstLP5253kfwXMTbX8XvrYENek3FKp6F_C4GboosFaRKv2G6bvVYIORJfXvXlOzSDwlaxMDn8FuT3Cc1KoaHoF_yK-rgQFCLPPqcfGlJaTHuDMz1dS_Cj40mlNHBHJC5I-Y3vgrdrHmz1_31AwVGAmMyrQanNAp8daazhVVngA7xaUNqiF-18f3bHElgfiIF0JRqD5SLmCO_OQj22HKZhRk2g2Xq0U6EmrHTyiN5RXu1VzEOjrb_7RZy2fUsgQ")
+
+//		Handler(ww, rr)
+//	}
 func Handler(w http.ResponseWriter, r *http.Request) { //对下游的请求r进行响应w
 	// httpProxy := os.Getenv("http_proxy")
 	// accessToken = os.Getenv("ACCESS_TOKEN")
@@ -151,7 +175,12 @@ func Handler(w http.ResponseWriter, r *http.Request) { //对下游的请求r进�
 		return
 	}
 	//对上游返回的有效响应进行处理 w-----------------------------------------------------------------
-	print(response.Body) ////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// body, err := ioutil.ReadAll(response.Body)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// print(string(body))
 	// var full_response string
 	// // for i := 2; i > 0; i-- {
 	// var continue_info *chatgpt.ContinueInfo
@@ -187,8 +216,8 @@ func Handler(w http.ResponseWriter, r *http.Request) { //对下游的请求r进�
 	// }
 	// }
 
-	if !original_request.Stream {
-		// response := official_types.NewChatCompletion(full_response) //完成非流回复
+	if !original_request.Stream { //完成非流回复
+		// response := official_types.NewChatCompletion(full_response) //以官方格式回复
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(response)
@@ -245,7 +274,7 @@ func POSTconversation(message ChatGPTRequest, access_token string) (*fhttp.Respo
 	// 	client.SetProxy(http_proxy)
 	// }
 
-	// client.SetProxy("http://127.0.0.1:7890") //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
+	client.SetProxy("http://127.0.0.1:7890") //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
 	apiUrl := "https://chat.openai.com/backend-api/conversation"
 	// if API_REVERSE_PROXY != "" {
 	// 	apiUrl = API_REVERSE_PROXY
